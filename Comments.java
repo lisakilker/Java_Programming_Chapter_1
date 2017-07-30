04407eb9cbff68e5dd71666e1708eb7da7c157b5
Lisa Kilker <lisakilker@gmail.com>
2017-07-30 16:44:40 +0200

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package W1D1;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Lisa
+ */
+public class Comments {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+       System.out.print("Program comments are nonexecuting statements you add to a file for the purpose of documentation!"); 
+//This is the first comment
+/*This is the second comment
+*/
+/** This is the third comment
+ * 
+ */
+    }
+}
